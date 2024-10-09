7e76bbee541a9c594885e9880eba139470d80c73
Mii-Muu <spotinfotech.in@gmail.com>
2024-10-09 13:36:25 +0530

    declared error variable

--- src/dbConnect/dbConnect.js
+++ src/dbConnect/dbConnect.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     const connection = mongoose.connection;
     return connection;
   } catch (error) {
-    console.log("Error connecting to the database !");
+    console.log("Error connecting to the database!", error);
   }
 }
