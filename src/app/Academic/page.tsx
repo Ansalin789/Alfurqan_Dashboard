0d31c22938b69d04ed995333ab82a18bc71f111e
Mii-Muu <spotinfotech.in@gmail.com>
2024-11-18 19:13:00 +0530

    sdssf

--- src/app/Academic/page.tsx
+++ src/app/Academic/page.tsx
@@ -3,10 +3,6 @@
 import React from 'react'
 import 'react-calendar/dist/Calendar.css'
 import BaseLayout1 from '@/components/BaseLayout1'
-
-// import { FaUser } from 'react-icons/fa';
-// import { FaCalendarAlt } from 'react-icons/fa';
-// import { FaClock } from 'react-icons/fa';
 import { FaUserAlt } from 'react-icons/fa';
 import { AiOutlineClockCircle } from 'react-icons/ai';
 import { Doughnut } from 'react-chartjs-2';
