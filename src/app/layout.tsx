751f9f3a32373b92b86ecbce618c080191c01468
Jothika-Selvaraj <jothikaselvaraj8787@gmail.com>
2025-01-06 14:43:46 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/context/ThemeContext';
 
 
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'],  display: 'swap', });
 
 export const metadata: Metadata = {
   title: "Alfurqan Academy",
