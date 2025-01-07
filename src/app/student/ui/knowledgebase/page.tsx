337d9712aac3e1b8c55baf9fe4c88c8c10a28279
Mii-Muu <spotinfotech.in@gmail.com>
2025-01-07 15:46:27 +0530

    knowledge commit

---
+++ src/app/student/ui/knowledgebase/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+
+import React from 'react';
+import PdfCard from '@/app/student/components/knowlegdebase/PdfCard';
+import BaseLayout2 from '@/components/BaseLayout2';
+import { FiFilter } from "react-icons/fi";
+import { GrShare } from "react-icons/gr";
+import RecordedClassesBase from '../../components/knowlegdebase/RecordedClassesBase';
+
+
+
+const Knowledgebase: React.FC = () => {
+  return (
+    <BaseLayout2>
+        <div className="p-6">
+            <section className=" bg-[#ffffff] p-6 py-3 rounded-xl">
+                <div className="flex items-center justify-between p-1 border-b-2 border-b-[#525151]">
+                    <h2 className="text-[25px] font-bold text-[#5C5F85]">Knowledge Base</h2>
+                    {/* Date and Filter */}
+                    <div className="flex items-center space-x-2 ">
+                    <div className="relative">
+                    <input
+                        type="text"
+                        placeholder="MM/DD/YYYY"
+                        className="border border-gray-300 rounded-lg px-4 py-2 text-sm w-full pr-10"
+                    />
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-gray-500"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8 7V3m8 4V3m-9 8h10m-9 4h4m-7-6h16a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2z"
+                        />
+                        </svg>
+                    </div>
+                    </div>
+                        <button >
+                        <FiFilter className="w-6 h-6 text-[#687E9C]"/>
+                        </button>
+                    </div>
+                </div>
+                {/* <p className='text-end text-[#525151] text-[11px]'>view all ᐳ</p> */}
+
+                {/* Cards Grid */}
+                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 overflow-y-scroll scrollbar-thin scrollbar-track-black">
+                {[...Array(6)].map((_, index) => (
+                    <PdfCard
+                    title="Sample PDF"
+                    details="Details"
+                    pdfUrl="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" // Path to the PDF file
+                  />
+                  
+                ))}
+                </div>
+            </section>
+
+            <RecordedClassesBase />
+
+            <section>
+                <h2 className="text-[25px] font-bold text-[#5C5F85] p-2 px-7">
+                Learn More Courses
+                </h2>
+                <button className="bg-[#223857] flex text-white text-[10px] p-2 ml-4 px-7 text-sm rounded-full hover:bg-[#1f334e]">
+                <GrShare className='mt-[2px]'/>&nbsp; Look More Courses
+                </button>
+            </section>
+        </div>
+    </BaseLayout2>
+  );
+};
+
+export default Knowledgebase;
--- src/components/Sidebar2.tsx
+++ src/components/Sidebar2.tsx
@@ -3,19 +3,19 @@
 import Image from "next/image";
 import Link from 'next/link';
 import React from 'react';
-import { RiDashboardFill } from "react-icons/ri";
-import { MdContactSupport } from "react-icons/md";
+import { MdContactSupport, MdOutlinePayment, MdAssignment } from "react-icons/md";
 import { IoPeopleSharp } from "react-icons/io5";
-import { MdAssignment } from "react-icons/md";
-import { MdOutlinePayment } from "react-icons/md";
-import { GiGiftOfKnowledge } from "react-icons/gi";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { FaFolderOpen } from "react-icons/fa";
+
+
 import "@/styles/globals.css";
 
 const SidebarItems = [
     {
         name: 'Dashboard',
         href: '/Student',
-        icon: RiDashboardFill,    
+        icon: TbLayoutDashboardFilled,    
     },
     {
         name: 'Classes',
@@ -35,7 +35,7 @@ const SidebarItems = [
     {
         name: 'Knowledge',
         href: '/Student/knowledge',
-        icon: GiGiftOfKnowledge,
+        icon: FaFolderOpen,
     },
     {
         name: 'Support',
@@ -46,10 +46,10 @@ const SidebarItems = [
 
 export default function Sidebar2() {
     return (
-        <div className="sidebar__wrapper">
-            <aside className='sidebar'>
+        <div className="sidebar__wrapper bg-[#012A4A]">
+            <aside className='sidebar bg-[#012A4A]'>
                 <div className='sidebar__top'>
-                    <Image src="/assets/images/alfwhite.png" width={100} height={100} className='sidebar__logo text-center bg-cover bg-center' alt='logo'/>
+                    <Image src="/assets/images/whitelogo.png" width={100} height={100} className='sidebar__logo text-center bg-cover bg-center' alt='logo'/>
                     <p className='sidebar__logo-name'>
                         <Image src="/assets/images/alf2.png" width={130} height={50} alt='logo'/>
                     </p>
@@ -66,6 +66,26 @@ export default function Sidebar2() {
                         </li>
                     ))}
                 </ul>
+                <div className="mt-4 border-[1px] border-b-[#9882BB]"></div>
+
+                <Image src="/assets/images/refer.png" width={180} height={180} className='text-center bg-cover bg-center w-28 justify-center ml-8' alt='logo'/>
+
+                <button className="relative ml-6 mt-3 flex items-center justify-center px-8 py-3 bg-gradient-to-b from-[#E63C48] via-[#EE693A] to-[#F9A826] text-white font-bold text-sm rounded-full shadow-lg hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400">
+                <span className="pr-2 text-[13px]">Upgrade</span>&nbsp;
+                <span className="absolute ml-2 right-3 flex items-center justify-center">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    fill="red"
+                    stroke="yellow"
+                    strokeWidth="2"
+                    >
+                    <polygon points="12,2 15,9 22,9 17,14 19,21 12,17 5,21 7,14 2,9 9,9" />
+                    </svg>
+                </span>
+                </button>
+
             </aside>
         </div>
     );
