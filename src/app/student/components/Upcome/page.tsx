3f673a87ef1e200abc20ec75080f850fbdf74507
rahul rm <rahulramachandiren@gamil.com>
2025-02-11 15:41:40 +0530

    totalhours and upcoming error fix

--- src/app/student/components/Upcome/page.tsx
+++ src/app/student/components/Upcome/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from "react";
 import { Calendar, User } from "lucide-react";
 import axios from "axios";
@@ -55,7 +56,7 @@ const UpcomingClasses = () => {
           },
         });
 
-        const sortedClasses = response.data.classSchedule.sort(
+        const sortedClasses = response.data.classSchedule.toSorted(
           (a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
         );
         
