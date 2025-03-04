e6014ac4e862a8a18240ce89e4212e1957b7bfff
Ansalin789 <ansalinsagar789@gmail.com>
2025-03-05 01:05:55 +0530

    recent commit by jijo

---
+++ src/app/admin-main/ui/dashboard/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React from "react";
+import "react-calendar/dist/Calendar.css";
+import BaseLayout4 from "@/components/BaseLayout4";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import TotalList from "@/components/Academic/TotalList";
+import NextEvaluationClass from "@/components/Academic/NextEvaluationClass";
+import TeachersStudent from "@/components/Academic/TeachersStudent";
+import Countries from "@/components/Academic/Countries";
+import Teachers from "@/components/Academic/Teachers";
+import Calender from "@/components/Academic/Calender";
+import UpcomingClasses from "@/components/Academic/UpcommingClasses";
+import Dashboardevaluation from "@/components/Academic/Dashboardevaluation";
+import { Search, Sun, Bell } from "lucide-react";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+const page = () => {
+  return (
+    <div>
+      <BaseLayout4>
+        <div className="p-2 w-[100%] mx-auto">
+          <div className="p-0 flex items-center justify-between mr-12">
+            <div className="relative w-1/4 left-3">
+              <Search
+                className="absolute left-6 top-1/2 transform -translate-y-1/2 text-gray-400"
+                size={13}
+              />
+              <input
+                type="text"
+                placeholder="Search here..."
+                className="h-9 w-60 pl-10 text-sm text-[#35324B] bg-[#E1E5EA] rounded-[10px] shadow-[0_4px_6px_rgba(0,0,0,0.2)] outline-none focus:ring-2 focus:ring-[#9CA3AF]"
+              />
+            </div>
+
+            <div className="flex items-center gap-4">
+              <button className="p-2 bg-[#CED4DC] rounded-lg shadow hover:bg-gray-200">
+                <Sun size={16} className="text-black" />
+              </button>
+              <button className="p-2 bg-[#CED4DC] rounded-lg shadow hover:bg-gray-200">
+                <Bell size={16} className="text-black" />
+              </button>
+              {/* <button className="p-2 bg-white rounded-lg shadow hover:bg-gray-200">
+                    <Settings size={18} className="text-gray-600" />
+                  </button> */}
+              {/* <Link href="#">
+                <img
+                  src="/assets/images/student-profile.png" // Replace with the actual image URL
+                  alt="Profile"
+                  className="w-8 h-8 rounded-lg border border-gray-300 shadow"
+                />
+              </Link> */}
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row mt-2">
+            <div className="flex-1 p-2">
+              <main className="grid grid-cols-12 gap-4 pr-20">
+                <div className="col-span-12 grid grid-cols-1 p-0">
+                  <TotalList />
+                </div>
+                <div className="col-span-12 grid grid-cols-1 p-0">
+                  <NextEvaluationClass />
+                </div>
+
+                <div className="col-span-5 grid grid-cols-1 p-0">
+                  <TeachersStudent />
+                </div>
+                <div className="col-span-4 grid grid-cols-1 p-0">
+                  <Countries />
+                </div>
+                <div className="col-span-3 grid grid-cols-1 p-0">
+                  <Teachers />
+                </div>
+                <div className="col-span-12 grid grid-cols-1 p-0">
+                  <Dashboardevaluation />
+                </div>
+              </main>
+            </div>
+
+            <div className="lg:w-[250px] lg:mt-2 rounded-[20px] pr-14">
+              <div className="pr-0 mb-0 rounded-[20px]">
+                <Calender />
+              </div>
+              <div className="pr-0 rounded-lg w-64 mt-11 -ml-12">
+                <UpcomingClasses />
+              </div>
+            </div>
+          </div>
+        </div>
+      </BaseLayout4>
+    </div>
+  )
+}
+
+export default page
--- src/components/Sidebar4.tsx
+++ src/components/Sidebar4.tsx
@@ -4,10 +4,14 @@ import Image from "next/image";
 import Link from 'next/link';
 import React from 'react';
 import { RiDashboardFill } from "react-icons/ri";
-import { MdContactSupport, MdAssignment } from "react-icons/md";
+import { MdBookmarks, MdAnalytics } from "react-icons/md";
 import { IoPeopleSharp } from "react-icons/io5";
-import { FaBookOpenReader } from "react-icons/fa6";
 import { LuMessagesSquare } from "react-icons/lu";
+import { GiGraduateCap } from "react-icons/gi";
+import { PiBookOpenFill } from "react-icons/pi";
+import { IoMdSettings } from "react-icons/io";
+
+
 
 
 
@@ -22,47 +26,47 @@ const SidebarItems = [
     {
         name: 'Evaluation',
         href: '/admin-main/ui/evaluation',
-        icon: IoPeopleSharp,
+        icon: MdBookmarks,
     },
     {
         name: 'Students',
         href: '/admin-main/ui/students',
-        icon: MdAssignment,
+        icon: GiGraduateCap,
     },
     {
         name: 'Employees',
         href: '/admin-main/ui/employees',
-        icon: FaBookOpenReader,
+        icon: IoPeopleSharp,
     },
     {
         name: 'Courses',
         href: '/admin-main/ui/courses', 
-        icon: LuMessagesSquare,
+        icon: PiBookOpenFill,
     },
     {
         name: 'Classes',
         href: '/admin-main/ui/classes',
-        icon: MdContactSupport,
+        icon: '/assets/images/clssss.png',
     },
     {
         name: 'Invoice',
         href: '/admin-main/ui/invoice',
-        icon: MdContactSupport,
+        icon: '/assets/images/invoicee.jpeg',
     },
     {
         name: 'Analytics',
         href: '/admin-main/ui/analytics',
-        icon: MdContactSupport,
+        icon: MdAnalytics,
     },
     {
         name: 'Messages',
         href: '/admin-main/ui/messagess',
-        icon: MdContactSupport,
+        icon: LuMessagesSquare,
     },
     {
         name: 'Settings',
         href: '/admin-main/ui/settingss',
-        icon: MdContactSupport,
+        icon: IoMdSettings,
     }
 ];
 
@@ -70,15 +74,19 @@ export default function Sidebar4() {
     return (
         <div className="sidebar__wrapper bg-[#012A4A] h-[100vh]">
             <aside className='sidebar bg-[#012A4A] shadow-lg'>
-                <div className='sidebar__top'>
+                <div className='justify-center align-middle mt-2 p-4'>
                 <Image src="/assets/images/whitelogo.png" width={150} height={150} className='bg-cover bg-center' alt='logo'/>
                 </div>
-                <ul className="mt-10">
+                <ul className="ml-6">
                     {SidebarItems.map(({ name, href, icon: Icon }) => (
                         <li className="text-center justify-center hover:no-underline hover:flex hover:bg-[#476a9b] hover:text-[#fff] hover:align-middle hover:justify-center] hover:pl-2 pl-2 py-2 hover:rounded-lg" key={name}>
-                            <Link href={href} className='no-underline flex align-middle justify-start w-[100%] text-[#fff] pt-[15px] pb-[15px] text-[15px]'>
+                            <Link href={href} className='no-underline flex align-middle justify-start w-[100%] text-[#fff] pt-[10px] pb-[10px] text-[14px]'>
                                 <span className="text-[20px] inline-block mr-[10px]">
-                                    <Icon />
+                                    {typeof Icon === 'string' ? (
+                                        <Image src={Icon} width={20} height={20} alt={name} />
+                                    ) : (
+                                        <Icon />
+                                    )}
                                 </span>
                                 <span className="sidebar__name">{name}</span>
                             </Link>
