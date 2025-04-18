10afdb6917b752cdf1f6cc843da1ab812a38a8c3
rahul rm <rahulramachandiren@gmail.com>
2025-04-18 12:03:45 +0530

    slidebar complted

--- src/components/BaseLayout4.tsx
+++ src/components/BaseLayout4.tsx
@@ -8,12 +8,12 @@ export default function BaseLayout4({ children }: Props) {
     return (
         <div className="flex h-screen">
         {/* Sidebar - only visible from md and up */}
-        <div className="hidden md:block md:w-54 bg-[#012A4A]">
+        <div className="hidden md:block md:w-50 bg-[#012A4A]">
           <Sidebar4 />
         </div>
       
         {/* Main content */}
-        <div className="flex-1 overflow-auto">
+        <div className="flex-1 overflow-auto p-4">
           {children}
         </div>
       </div>
