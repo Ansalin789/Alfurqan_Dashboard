ae20049e69dd36471c35a19e02103eec02984abe
Janni30 <jananipriyam52002@gmail.com>
2025-04-18 16:32:39 +0530

    student

--- src/app/admin-main/components/studenttab.tsx
+++ src/app/admin-main/components/studenttab.tsx
@@ -168,7 +168,7 @@ const TabbedTable = () => {
   return (
     <div className=" overflow-x-auto mt-4 bg-white shadow-md rounded-lg p-3 ">
       {/* Tabs */}
-      <div className="flex space-x-4 border-b pb-1 text-sm text-black mb-2">
+      <div className="flex space-x-4  pb-1 text-sm text-black mb-2">
         {tabs.map((tab) => (
           <button
             key={tab}
@@ -186,8 +186,8 @@ const TabbedTable = () => {
 
       {/* Table for 'Class' Tab */}
       {activeTab === "Class" && (
-        <div className="p-4">
-          <div className="overflow-x-auto bg-white rounded-lg border-2 border-[#1C3557] w-full max-w-[1255px] mx-auto  ">
+        <div className="p-1">
+          <div className="overflow-x-auto bg-white rounded-lg border-2 border-[#1C3557] w-full mx-auto  ">
             <table className="w-full text-[12px]">
               <thead className="border-b-[1px] border-[#1C3557] text-[11px] font-semibold">
                 <tr>
@@ -264,7 +264,7 @@ const TabbedTable = () => {
               if (item.label === "Attendance") {
                 suffix = "%";
               } else if (item.label === "Duration") {
-                suffix = " HRS";
+                suffix = "hrs";
               }
 
               return (
@@ -280,12 +280,12 @@ const TabbedTable = () => {
                         strokeLinecap: "round",
                       })}
                     />
-                    <div className="absolute text-md font-bold text-black">
+                    <div className="absolute text-sm font-semibold text-black">
                       {item.value}
                       {suffix}
                     </div>
                   </div>
-                  <p className="mt-2 text-sm font-medium">{item.label}</p>
+                  <p className="mt-4 text-xs font-medium">{item.label}</p>
                 </div>
               );
             })}
