77a1c7ccb19c6a6c03f162424fb30bdbfbc37bac
rahulrm00 <rahulramachandiren@gmail.com>
2025-06-09 18:40:53 +0530

    support page altered

--- src/app/Academic/support/page.tsx
+++ src/app/Academic/support/page.tsx
@@ -1,118 +1,151 @@
 import BaseLayout1 from "@/components/BaseLayout1";
+
 import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { MdOutlineArrowOutward } from "react-icons/md";
+
 
 const Support = () => {
   return (
     <BaseLayout1>
-      <div className="p-2 mx-auto">
-        {/* Page Title */}
-        <h1 className="mb-0 p-2 text-[25px] font-semibold text-gray-800">
-          Support
-        </h1>
-
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="col-span-1 p-0 h-[10vh]">
-            <div className="bg-[#4CBC9A] shadow-lg rounded-xl p-2">
-              <h2 className="text-[18px] font-semibold text-gray-800 mb-1 p-6">
-                Contact Details
-              </h2>
-              <div className="text-gray-800 space-y-4 justify-evenly px-6">
-                <p>
-                  <strong className="text-white text-[13px] font-semibold">
-                    Visit Us:
-                  </strong>
+   
+      <div className="p-4 mx-auto">
+        <div className="flex gap-x-5 w-auto">
+          <div className="bg-[#7689BD] shadow-lg rounded-xl p-4 h-[616px] w-[340px]">
+            <h2 className="text-[20px] font-semibold text-[#fff] mb-4 p-4">
+              Contact
+            </h2>
+            <div className="text-gray-800 space-y-4 justify-evenly px-6">
+              <p>
+                <strong className="text-white text-[16px] font-medium flex items-center gap-2">
+                  <img
+                    src="/assets/images/location.png"
+                    alt="location"
+                    className="w-4 h-4"
+                  />
+                  Visit Us :
+                </strong>
+                <div className="ml-6">
+                <span className="text-[13px] font-normal text-white">
+                  Come say Hello at Our Office HQ
+                </span>
+                <br />
+                <span className="text-white text-[13px] font-normal">
+                  {" "}
+                  128, City Road, London, EC1V 2NX, United Kingdom
+                </span>
+                </div>
+                
+              </p>
+              <p>
+                <strong className="text-white text-[16px] font-medium flex items-center gap-2">
+                  <img
+                    src="/assets/images/call.png"
+                    alt="call"
+                    className="w-4 h-4"
+                  />
+                  Call Us :
+                </strong>
+                <div className="ml-6">
+                <span className="text-[13px] font-normal text-white">
+                  Monday – Sunday/ 24×7
+                </span>
+                <br />
+                <span className="text-white text-[13px] font-normal">
+                  UK +44 20 4577 1227
                   <br />
-                  <span className="text-[11px] font-medium">
-                    Come say Hello at Our Office HQ
-                  </span>
-                  <br />
-                  <span className="text-white text-[11px] font-normal">
-                    {" "}
-                    128, City Road, London, EC1V 2NX, United Kingdom
-                  </span>
-                </p>
-                <p>
-                  <strong className="text-white text-[13px] font-semibold">
-                    Call Us:
-                  </strong>
-                  <br />
-                  <span className="text-[11px] font-medium">
-                    Monday – Sunday/ 24×7
-                  </span>
-                  <br />
-                  <span className="text-white text-[11px] font-medium">
-                    UK +44 20 4577 1227
-                    <br />
-                    USA +1 85 5442 3380
-                  </span>
-                </p>
-                <p>
-                  <strong className="text-white text-[13px] font-semibold">
-                    Chat to Us:
-                  </strong>
-                  <br />
-                  <span className="text-[11px] font-medium">
-                    Our Friendly team is here to Help
-                  </span>
-                  <br />
-                  <span className="text-white text-[11px] font-medium">
-                    contact@alfurqan.academy
-                  </span>
-                </p>
-              </div>
-            </div>
-            <div className="p-1 mt-4">
-              <div className="mt-0 text-center p-3">
-                <h3 className="text-lg font-semibold text-[#1C3557]">
-                  WE’RE HERE TO ASSIST YOU!
-                </h3>
-              </div>
-              <div className="justify-center align-middle text-center p-5">
-                <img
-                  src="/assets/images/support.png"
-                  alt=""
-                  className="w-60 justify-center ml-12"
-                />
-              </div>
+                  USA +1 85 5442 3380
+                </span>
+                </div>
+              </p>
+              <p>
+                <strong className="text-white text-[16px] font-medium flex items-center gap-2">
+                  <img
+                    src="/assets/images/email.png"
+                    alt="email"
+                    className="w-4 h-4"
+                  />
+                  Email to Us :
+                </strong>
+                <div className="ml-6">
+                <span className="text-[13px] font-normal text-white">
+                  Our Friendly team is here to Help
+                </span>
+                <br />
+                <span className="text-white text-[13px] font-normal">
+                  contact@alfurqan.academy
+                </span>
+                </div>
+              </p>
             </div>
           </div>
 
-          <div className="col-span-1 bg-[#223857] shadow-lg rounded-xl p-6 w-full max-w-[400px] h-[570px]">
-            <h2 className="text-[14px] text-center font-semibold text-[#fff] mb-6">
+          <div className="bg-[#5E6578] shadow-lg rounded-xl p-6 w-[915px] h-[616px]">
+            <h2 className="text-[20px] p-4 text-left font-semibold text-[#FAFAFA] mb-7">
               Do you have questions?
             </h2>
-            <details className="mb-4 p-2 w-full border-b border-b-[#fff]">
-              <summary className="text-[13px] font-medium text-[#fff] cursor-pointer">
-                What if I encounter technical issues?
-              </summary>
-              <div className="text-[#fff] text-[11px] mt-2 break-words">
-                Visit the Help Center or Support section of the site. You can
-                often find troubleshooting guides or contact the support team
-                via email, chat, or phone.
-              </div>
-            </details>
-            <details className="p-2 mb-4 w-full border-b border-b-[#fff]">
-              <summary className="text-[13px] font-medium text-[#fff] cursor-pointer">
-                Can I use Teacher communicate with students?
-              </summary>
-              <div className="text-[#fff] text-[11px] mt-2 break-words">
-                Yes, the Teacher Module provides features to communicate
-                directly with students.
-              </div>
-            </details>
-            <details className="p-2 w-full border-b border-b-[#fff]">
-              <summary className="text-[13px] font-medium text-[#fff] cursor-pointer">
-                Can I track my teacher progress?
-              </summary>
-              <div className="text-[#fff] text-[11px] mt-2 break-words">
-                Yes, the Teacher progress to communicate directly with students.
-              </div>
-            </details>
+            <div className="px-6">
+              <details className="group mb-6 py-2 w-full border-b border-b-[#818795]">
+                <summary className="flex items-center justify-between text-[16px] font-normal text-[#fff] cursor-pointer">
+                  What if I encounter technical issues?
+                  <IoIosArrowDown className="text-white transition-transform duration-300 group-open:rotate-180" />
+                </summary>
+                <div className="text-[#BBBBBB] text-[14px] mt-2 break-words">
+                  Visit the Help Center or Support section of the site. You can
+                  often find troubleshooting guides or contact the support team
+                  via email, chat, or phone.
+                </div>
+              </details>
 
-            <div className="flex gap-3 mt-10 text-[#fff] text-center p-2">
-              <p className="p-2">My question is not here</p>
-              <button className="bg-slate-950 p-2 rounded-lg">
+              <details className="group py-2 mb-6 w-full border-b border-b-[#818795]">
+                <summary className="flex items-center justify-between text-[16px] font-normal text-[#fff] cursor-pointer">
+                  Can I use the Teacher Module to communicate with students?
+                  <IoIosArrowDown className="text-white transition-transform duration-300 group-open:rotate-180" />
+                </summary>
+                <div className="text-[#BBBBBB] text-[14px] mt-2 break-words">
+                  Yes, the Teacher Module provides features to communicate
+                  directly with students.
+                </div>
+              </details>
+
+              <details className="group py-2 mb-6 w-full border-b border-b-[#818795]">
+                <summary className="flex items-center justify-between text-[16px] font-normal text-[#fff] cursor-pointer">
+                  Can I track my students' progress?
+                  <IoIosArrowDown className="text-white transition-transform duration-300 group-open:rotate-180" />
+                </summary>
+                <div className="text-[#BBBBBB] text-[14px] mt-2 break-words">
+                  Yes, the Teacher Module allows tracking student progress
+                  effectively.
+                </div>
+              </details>
+
+              <details className="group py-2 mb-6 w-full border-b border-b-[#818795]">
+                <summary className="flex items-center justify-between text-[16px] font-normal text-[#fff] cursor-pointer">
+                  Does the platform support mobile access?
+                  <IoIosArrowDown className="text-white transition-transform duration-300 group-open:rotate-180" />
+                </summary>
+                <div className="text-[#BBBBBB] text-[14px] mt-2 break-words">
+                  Yes, the platform is fully responsive and can be accessed from
+                  any mobile device.
+                </div>
+              </details>
+
+              <details className="group py-2 mb-6 w-full border-b border-b-[#818795]">
+                <summary className="flex items-center justify-between text-[16px] font-normal text-[#fff] cursor-pointer">
+                  Can I provide personalized feedback to students?
+                  <IoIosArrowDown className="text-white transition-transform duration-300 group-open:rotate-180" />
+                </summary>
+                <div className="text-[#BBBBBB] text-[14px] mt-2 break-words">
+                  Yes, you can provide personalized feedback to students through
+                  the platform.
+                </div>
+              </details>
+            </div>
+
+            <div className="flex gap-3 mt-32 text-[#fff] text-center justify-end p-2">
+              <button className="bg-[#FAFAFA] text-[#1B242C] p-2 rounded-lg flex items-center gap-2">
                 Connect Us
+                <MdOutlineArrowOutward className="w-4 h-4" />
               </button>
             </div>
           </div>
