baef00276feed0a206d87bb4ce83d1a0cc98a234
Janni30 <jananipriyam52002@gmail.com>
2025-06-19 15:51:11 +0530

    mamage

--- src/app/Academic-coach/ui/managestudents/page.tsx
+++ src/app/Academic-coach/ui/managestudents/page.tsx
@@ -11,6 +11,8 @@ import Modal from "react-modal";
 
 export interface Student {
   _id: string;
+  teacherName: string;
+  sessionClassType: string;
   username: string;
   password: string;
   role: string;
@@ -530,15 +532,16 @@ const ManageStudents = () => {
                         <td className="px-3 py-2 text-[#3D8FDE] font-medium">
                           {item.username}
                         </td>
-                        <td className="px-3 py-2"></td>
-                        <td className="px-3 py-2"></td>
+                        <td className="px-3 py-2">{item.teacherName}</td>
+                        <td className="px-3 py-2">{item.sessionClassType}</td>
                         <td className="px-3 py-2">
                           {item.student.studentPhone}
                         </td>
+                        
                         <td className="px-3 py-2 whitespace-nowrap">
                           {item.classScheduleCount}
                         </td>
-                        <td className="px-3 py-2"></td>
+                        <td className="px-3 py-2">1</td>
                         <td className="relative px-3 py-2">
                           <button
                             className="p-1"
