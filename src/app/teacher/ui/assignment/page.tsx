e8fcaa0841f3e72a3f699b10d03118dd29a4bf5c
Janni30 <jananipriyam52002@gmail.com>
2025-06-26 18:52:07 +0530

    assign

--- src/app/teacher/ui/assignment/page.tsx
+++ src/app/teacher/ui/assignment/page.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import Assignment from '../../components/Assignment'
-import StudentList from '../../components/StudentsList'
-import BaseLayout from '@/components/BaseLayout'
+import React from 'react';
+import Assignment from '../../components/Assignment';
+import StudentList from '../../components/StudentsList';
+import BaseLayout from '@/components/BaseLayout';
+import TeacherHeader from '../../components/TeacherHeader';
 
 const page = () => {
   return (
     <BaseLayout>
-      <div className='p-4 mx-auto w-[1250px] pr-16'>
+        <TeacherHeader currentSection="Assignments" />
+
+      {/* Page wrapper */}
+      <div className=" min-h-screen py-px-4 md:px-8">
+        {/* Dashboard Title */}
+
+        {/* Assignment Cards Section */}
+        <div className="mb-5">
           <Assignment />
+        </div>
+
+        {/* Student List Section */}
+        <div>
           <StudentList />
+        </div>
       </div>
     </BaseLayout>
-  )
-}
+  );
+};
 
-export default page
+export default page;
