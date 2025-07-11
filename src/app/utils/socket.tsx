b22eff6a2ce517bb0d81f13739445f6cd2d1132b
rahul rm <rahulramachandiren@gmail.com>
2025-07-11 10:49:08 +0530

    socket url

--- src/app/utils/socket.tsx
+++ src/app/utils/socket.tsx
@@ -5,7 +5,7 @@ let socket: Socket | null = null;
 
 export const getSocket = (userId: string): Socket => {
   if (!socket) {
-    socket = io('http://localhost:5001', {
+    socket = io('https://api.blackstoneinfomaticstech.com', {
       transports: ['websocket'],
       withCredentials: true,
       reconnection: true,
