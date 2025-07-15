dbec1708bd0fc4a375d5d9f8636596685008adf3
Ansalin789 <ansalinsagar789@gmail.com>
2025-07-15 11:18:18 +0530

    question auth

--- src/app/teacher/ui/question/page.tsx
+++ src/app/teacher/ui/question/page.tsx
@@ -99,7 +99,13 @@ export default function Page() {
       if(!assigmnetId) return;
       try {
         const res = await axios.get<Assignment>(
-          `http://localhost:5001/assignments/${assigmnetId}`
+          `http://localhost:5001/assignments/${assigmnetId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         setAssignments(res.data);
       } catch (error) {
