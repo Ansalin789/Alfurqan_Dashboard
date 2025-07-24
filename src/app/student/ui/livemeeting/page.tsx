1be3fccca89cdd971f03a48d5f630ab1faf59b33
master <sreclib0017@gmsail.com>
2025-07-24 17:31:06 +0530

    meet

--- src/app/student/ui/livemeeting/page.tsx
+++ src/app/student/ui/livemeeting/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { JitsiMeeting } from "@jitsi/react-sdk";
+import BaseLayout2 from "@/components/BaseLayout2";
 
 interface Teacher {
   teacherId: string;
@@ -192,7 +193,7 @@ const handleEndCall = async () => {
 
 
   return (
-    <BaseLayout>
+    <BaseLayout2>
       {/* <TeacherHeader currentSection="Live Meeting" /> */}
       <div className="flex h-screen">
         <div className="flex flex-col w-full min-h-screen px-4 sm:px-6 md:px-8">
@@ -317,7 +318,7 @@ const handleEndCall = async () => {
           </div>
         </div>
       </div>
-    </BaseLayout>
+    </BaseLayout2>
   );
 };
 
