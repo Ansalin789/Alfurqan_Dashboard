c70403efac764518a5bc59588600666f362c7df7
kathir63744 <kathir637445@gmail.com>
2026-06-18 15:21:12 +0530

    applied1

--- src/app/_components/contents/toast_message.ts
+++ src/app/_components/contents/toast_message.ts
@@ -32,16 +32,7 @@ export const AppSuccessToastMessages = Object.freeze({
   ASSIGNMENT_ASSIGNED: "Assignment assigned successfully",
 });
 
-    APPLICANT_CREATE: "Unable to create applicant",
 
-  INVALID_INPUTS: "Please check the input fields",
-
-  LOGIN_AGAIN: "Please login again",
-
-  NO_PERMISSION: "You don't have permission to perform this action",
-
-  SERVER_ERROR: "Internal server error",
-});
 
 export const appSuccessToastMessages = Object.freeze({
   SIGNIN: "Welcome back",
