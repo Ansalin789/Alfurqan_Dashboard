1edb8dc282176612bd6107ce87a16013cbcb4651
janni30 <jananipriyam52002@gmail.com>
2026-06-22 17:37:36 +0530

    teanat ui starts

---
+++ src/app/(super-admin)/super-admin/ui/tenants/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TenantStats from '../../components/Tenant-Cards'
+
+const page = () => {
+  return (
+    <div>
+      <TenantStats />
+    </div>
+  )
+}
+
+export default page
